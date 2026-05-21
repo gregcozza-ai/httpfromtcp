c26e49da3ee2eb4e38ecdc4d2e2e782b8de47da9
gregcozza-ai <greg.cozza@gmail.com>
2026-05-21 16:40:10 -0700

    channel refactor

--- main.go
+++ main.go
@@ -4,25 +4,50 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
-func main () {
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func () {
+		defer f.Close()
+		buffer := make([]byte, 8)
+		currentLine := ""
+
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				panic(err)
+			}
+
+			currentLine += string(buffer[:n])
+			parts := strings.Split(currentLine, "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				ch <- parts[i]
+			}
+
+			currentLine = parts[len(parts)-1]
+		}
+
+		if currentLine != "" {
+			ch <- currentLine 
+		}
+		close(ch)
+	}()
+	return ch 
+}
+
+func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-
-	buffer := make([]byte, 8)
-	for {
-		n, err := file.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			panic(err)
-		}
-		fmt.Printf("read: %s\n", string(buffer[:n]))
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
-
 }
